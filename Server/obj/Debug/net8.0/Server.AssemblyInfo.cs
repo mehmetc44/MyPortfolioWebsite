5c3be30060b794c57497d21b4b86fa86403fc34f
Mehmet CAKMAK <cakmakm541@gmail.com>
2026-07-14 14:02:12 +0300

    Home Page Zoom effect fixed

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b2a7e833c42964611c691dc8f544efb2ad7cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800ba752d37b7b44cd7af04f15c6ae0ac06a0b11")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
